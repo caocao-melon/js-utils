2d3d2acbc3a7f754c32c7e3a4ed9a08e4d6cd3b7
赵菁 <zhaojing@zhaojingdeMacBook-Pro.local>
2019-04-03 15:40:03 +0800

    feat(时间价格): rt

--- data.js
+++ data.js
@@ -8,18 +8,23 @@ function toThousands(num) {
   return num.replace(/(\d)(?=(?:\d{3})+$)/g, '$1,')
 }
 
-/**
- * 时间戳转日期
- * @param timestamp
- * @returns {string}
- */
-function formatDate(timestamp) {
-  var date = new Date(timestamp)
-  var Y = date.getFullYear() + '-'
-  var M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-'
-  var D = (date.getDate() < 10 ? '0' + (date.getDate()) : date.getDate()) + ' '
-  var h = (date.getHours() < 10 ? '0' + (date.getHours()) : date.getHours()) + ':'
-  var m = (date.getMinutes() < 10 ? '0' + (date.getMinutes()) : date.getMinutes()) + ':'
-  var s = (date.getSeconds() < 10 ? '0' + (date.getSeconds()) : date.getSeconds())
-  return Y + M + D + h + m + s
+// 时间戳转日期
+const getLocalTime = (nS) => {
+  return new Date(parseInt(nS) * 1000).toLocaleString().replace(/:\d{1,2}$/, ' ');
+}
+
+//价格转化 保留两位小数，0.123=>0.12 12=>12.00 
+const returnFloat = x => {
+  let f = parseFloat(x)
+  f = Math.round(x * 100) / 100
+  let s = f.toString()
+  let rs = s.indexOf('.')
+  if (rs < 0) {
+    rs = s.length
+    s += '.'
+  }
+  while (s.length <= rs + 2) {
+    s += '0'
+  }
+  return s
 }
